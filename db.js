d29cdc946d30eb2ef0503ed3df25d9fa0897d719
GarlicBuns19 <planterx26@gmail.com>
2022-12-29 10:11:51 +0200

    Added notes

--- db.js
+++ db.js
@@ -1,10 +1,23 @@
 const { MongoClient } = require("mongodb");
 
-let client = new MongoClient("mongodb://0.0.0.0:27017/bookstore", {
+/* instead of this
+const url = "mongodb://localhost:27017";
+
+Just Replace
+const url = "mongodb://0.0.0.0:27017";*/
+let url = "mongodb://0.0.0.0:27017/bookstore"
+
+let client = new MongoClient(url, {
+  // MongoTopologyClosedError: Topology is closed Fix Below
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// Connecting , put in async
 client.connect();
+
+// The whole database
 const db = client.db();
 
+// Exporting the db
 module.exports = {db}
